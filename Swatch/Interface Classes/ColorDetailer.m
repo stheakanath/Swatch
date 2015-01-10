c06de0de4cb6ec043e721843c73b21a5a30a6ed6
Sony Theakanath <stheakanath@gmail.com>
2015-01-10 00:17:54 -0800

    custom shape selector!

--- Swatch/Interface Classes/ColorDetailer.m
+++ Swatch/Interface Classes/ColorDetailer.m
@@ -11,17 +11,22 @@
 @implementation ColorDetailer
 
 - (id) init {
-    self = [super initWithFrame:CGRectMake(0, 0, 40, 40)];
+    self = [super initWithFrame:CGRectMake(0, 0, 60, 60)];
     [self setBackgroundColor:[UIColor blackColor]];
     self.layer.shadowOffset = CGSizeMake(0, 10);
     self.layer.shadowRadius = 5;
     self.layer.shadowOpacity = 0.5;
+    
+    UIImage *_maskingImage = [UIImage imageNamed:@"Color.png"];
+    CALayer *_maskingLayer = [CALayer layer];
+    _maskingLayer.frame = self.bounds;
+    [_maskingLayer setContents:(id)[_maskingImage CGImage]];
+    [self.layer setMask:_maskingLayer];
     return self;
 }
 
 - (void) changeColor:(UIColor*) color location:(CGPoint)point{
-    NSLog(@"%@", NSStringFromCGPoint(point));
-    [self setFrame:CGRectMake(point.x-20, point.y-60, 40, 40)];
+    [self setFrame:CGRectMake(point.x-40, point.y-70, 60, 60)];
     [self setBackgroundColor:color];
 }
 
