0a8e2ab584bd83710b6ee78fedba311fbfd3d07d
Sony Theakanath <stheakanath@gmail.com>
2015-01-14 19:49:11 -0800

    SUPER FAST NOW. #HELLYEAH

--- Swatch/MagnifierView.m
+++ Swatch/MagnifierView.m
@@ -24,52 +24,20 @@
 
 - (void)setTouchPoint:(CGPoint)pt {
 	touchPoint = pt;
-   self.layer.borderColor = [[self colorAtPosition:touchPoint] CGColor];
+    self.layer.borderColor = [[self colorAtPosition:touchPoint] CGColor];
 	self.center = CGPointMake(pt.x, pt.y-60);
 }
 
 - (UIColor *)colorAtPosition:(CGPoint)position {
-   CGRect sourceRect = CGRectMake(position.x, position.y, 1.f, 1.f);
-    UIGraphicsBeginImageContextWithOptions(viewToMagnify.bounds.size, viewToMagnify.opaque, viewToMagnify.contentScaleFactor);
-    CGContextRef context = UIGraphicsGetCurrentContext();
-    [self.viewToMagnify drawViewHierarchyInRect:self.viewToMagnify.bounds afterScreenUpdates:YES];
-    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
-    UIGraphicsEndImageContext();
-    
-    CGImageRef imageRef = CGImageCreateWithImageInRect(image.CGImage, sourceRect);
+    unsigned char pixel[4] = {0};
     CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
-    unsigned char *buffer = malloc(4);
-    CGBitmapInfo bitmapInfo = kCGImageAlphaPremultipliedLast | kCGBitmapByteOrder32Big;
-    context = CGBitmapContextCreate(buffer, 1, 1, 8, 4, colorSpace, bitmapInfo);
-    CGColorSpaceRelease(colorSpace);
-    CGContextDrawImage(context, CGRectMake(0.f, 0.f, 1.f, 1.f), imageRef);
-    CGImageRelease(imageRef);
-    CGContextRelease(context);
-    CGFloat r = buffer[0] / 255.f;
-    CGFloat g = buffer[1] / 255.f;
-    CGFloat b = buffer[2] / 255.f;
-    CGFloat a = buffer[3] / 255.f;
-    free(buffer);
-    return [UIColor colorWithRed:r green:g blue:b alpha:a];
-    
-   /* unsigned char pixel[4] = {0};
-    
-    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
-    
     CGContextRef context = CGBitmapContextCreate(pixel, 1, 1, 8, 4, colorSpace, kCGBitmapAlphaInfoMask & kCGImageAlphaPremultipliedLast);
-    
-    CGContextTranslateCTM(context, -point.x, -point.y);
-    
-    [self.layer renderInContext:context];
-    
+    CGContextTranslateCTM(context, -position.x, -position.y);
+    [self.viewToMagnify.layer renderInContext:context];
     CGContextRelease(context);
     CGColorSpaceRelease(colorSpace);
-    
-    //NSLog(@"pixel: %d %d %d %d", pixel[0], pixel[1], pixel[2], pixel[3]);
-    
     UIColor *color = [UIColor colorWithRed:pixel[0]/255.0 green:pixel[1]/255.0 blue:pixel[2]/255.0 alpha:pixel[3]/255.0];
-    
-    return color; */
+    return color;
 }
 
 - (void) changeColor:(UIColor*) color {
